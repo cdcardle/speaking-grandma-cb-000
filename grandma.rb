677855f9a8f30f5584bd48a869f20c41c15e5fca
Chris Cardle <cdcardle@gmail.com>
2017-10-26 14:39:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(string)
   if (string == "I LOVE YOU GRANDMA!")
-    puts "I LOVE YOU TOO PUMPKIN!"
+    returns "I LOVE YOU TOO PUMPKIN!"
   elsif (string == string.upcase)
-    puts "NO, NOT SINCE 1938!"
+    returns "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    returns "HUH?! SPEAK UP, SONNY!"
   end
 end
